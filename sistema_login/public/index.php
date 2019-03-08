60670b251d3bb4365bb3e2d1a645a3065623b22d
Davi Silva <davi@localhost.localdomain>
2019-03-08 18:45:13 -0300

    update file

--- sistema_login/public/index.php
+++ sistema_login/public/index.php
@@ -17,25 +17,7 @@ if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
 <a href="../app/exit.php"><i class="material-icons">exit</i>Sair</a>
 
 <div class="container">
-    <div class="row">
-        <div class="col s12 m7"style="width: 320px">
-            <h4>Welcome Here...</h4>
-            <div class="card">
-                <div class="card-image">
-                    <img src="img/perfil.png">
-                    <span class="card-title">Davi Silva</span>
-                </div>
-
-                <div class="card-content">
-                    <p>I'm a Analysis Systems and Wed Developer student with emphasis in HTML 5, CSS 3, Javascript and PHP...</p>
-                </div>
-
-                <div class="card-action">
-                    <a href="https://github.com/davi1985">My Github</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <h4>Welcome Here...</h4>
 </div>    
 
 <?php include 'footer.php'; ?>
